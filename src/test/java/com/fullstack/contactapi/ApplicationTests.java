704335fea4b0c0112bdc773bdc3206761d9132f0
jijimenezf <jijimenezf@hotmail.com>
2024-04-09 13:31:11 -0600

    Unit test

--- src/test/java/com/fullstack/contactapi/ApplicationTests.java
+++ src/test/java/com/fullstack/contactapi/ApplicationTests.java
@@ -1,13 +1,28 @@
 package com.fullstack.contactapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldReturnAllContactsWhenListIsRequested() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/contacts", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
